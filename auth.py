74bd23a07e05b94619161cbe5079948e1aaf63eb
Wu Zefeng <flamesdesperado@gmail.com>
2019-09-12 17:42:22 +0800

    Added more tokens

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 import requests
 
-APP_KEY = ["2815647836", "3291629583"]
-APP_SECRET = ["52fcbb03a2ec45d54fd5c4b40a9582ba", "b23db2906e93082f54b85a439fa0ddda"]
+APP_KEY = ["2815647836", "3291629583", "3859594497", "1570715685", "845176942", "1554698740"]
+APP_SECRET = ["52fcbb03a2ec45d54fd5c4b40a9582ba", "b23db2906e93082f54b85a439fa0ddda", "aefb24e6ee4ac8b5034ddd9f7d51a640", "6b00b75c912af063f8ccb27e0305f097", "2dc89c9f76ac8e2befc73a2fd83e67b9", "6899a278d277f5dd000a5d810c0b854c"]
 REDIRECT_URI = "https://api.weibo.com/oauth2/default.html"
 
 def get_code(i):
--- weibo_api.py
+++ weibo_api.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # Weibo API
 APP_KEY = "2815647836"
 APP_SECRET = "52fcbb03a2ec45d54fd5c4b40a9582ba"
-ACCESS_TOKENS = ["2.00UICb9GmcKYED184c921789UvEILB", "2.00UICb9GjN2laD17d8d8ed46m3MqiC"]
+ACCESS_TOKENS = ["2.00UICb9GmcKYED184c921789UvEILB", "2.00UICb9GjN2laD17d8d8ed46m3MqiC", "2.00UICb9GJq9MNEcfa105c155Ym6k5C", "2.00UICb9GxtYSiBb7f3f1cfff8pOxYB", "2.00UICb9G0KIRMv93790c41bd0IBVtN", "2.00UICb9GIK3NhB50cb97aab8gTTwQD"]
 WEIBO_API_TIME_FORMAT = "%a %b %d %H:%M:%S %z %Y"
 REQUEST_URL = "https://api.weibo.com/2/statuses/home_timeline.json"
 WEIBOLINK = "https://m.weibo.cn/status/"
@@ -21,7 +21,7 @@ def get_timeline():
     url = REQUEST_URL
     response_text = None
     for access_token in ACCESS_TOKENS:
-        get_params = {"access_token": access_token, "count": 100}
+        get_params = {"access_token": access_token}
         response = requests.get(url, params=get_params)
         if (response.status_code == 200):
             print("Using token: {} to query".format(access_token))
