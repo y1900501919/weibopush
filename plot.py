48fa08ca5e2ad0afbc69d97262d14417dc7cfaf0
Wu Zefeng <flamesdesperado@gmail.com>
2019-09-25 18:59:39 +0800

    bug fix

--- plot.py
+++ plot.py
@@ -7,4 +7,5 @@ def plot_polyline(x_names, values, save_path):
     plt.plot(x_names, values)
     plt.yticks(range(min(values), math.ceil(max(values))+1))
     plt.savefig(save_path)
+    plt.clf()
     return save_path
