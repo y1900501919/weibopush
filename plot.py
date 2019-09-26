8739e5edd493de3ac653f06fa29c2aa21346672e
Wu Zefeng <flamesdesperado@gmail.com>
2019-09-26 19:25:03 +0800

    typo fix

--- plot.py
+++ plot.py
@@ -8,9 +8,9 @@ from utils import date_to_str
 def plot_polyline(x_names, values, save_path):
     y_pos = np.arange(len(x_names))
     barlist = plt.bar(y_pos, values, align='center', alpha=0.5)
-    today_date = date_to_str(date_format='%m-%d')
-    yesterday_date = date_to_str(date_format='%m-%d', days_ago=1)
-    
+    today_date = date_to_str(date_format='%m/%d')
+    yesterday_date = date_to_str(date_format='%m/%d', days_ago=1)
+
     if today_date in x_names:
         barlist[x_names.index(today_date)].set_color('g')
     if yesterday_date in x_names:
